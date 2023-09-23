8716b6de01d338ef51b3eb7df80c3ebfc2533ad6
Aravind Allamneni <aravindallamneni@gmail.com>
2023-09-23 14:47:28 +0530

    coloured the signin button for mobile navigation

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -110,7 +110,11 @@ const Nav = () => {
                 ) : (
                     <>
                     {providers && Object.values(providers).map((provider) => (
-                        <button type="button" key={provider.name} onClick={() => signIn(provider.id)}>
+                        <button 
+                            type="button" key={provider.name}
+                            onClick={() => signIn(provider.id)}
+                            className='black_btn'
+                        >
                             Sign In
                         </button>
                     ))}
